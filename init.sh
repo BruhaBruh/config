0f99d61eab44c0dbb7b49699aa64797a30ec4acf
Dmitry Galiyakhbarov <46498219+BruhaBruh@users.noreply.github.com>
2022-11-03 09:45:52 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,13 +2,13 @@
 
 yarn
 yarn add -D @commitlint/config-conventional @commitlint/cli husky @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier prettier stylelint stylelint-config-prettier stylelint-config-standard
-npm set-script lint 'arn lint:eslint && yarn lint:stylelint'
+npm set-script lint 'yarn lint:eslint && yarn lint:stylelint'
 npm set-script lint:eslint 'eslint src'
 npm set-script lint:stylelint 'stylelint src'
-npm set-script format 'yarn format:prettier && yarn format:eslint && yarn format:stylelintyarn format:prettier && yarn format:eslint && yarn format:stylelint'
+npm set-script format 'yarn format:prettier && yarn format:eslint && yarn format:stylelint'
 npm set-script format:prettier 'prettier --write src'
 npm set-script format:eslint 'eslint --fix src'
-npm set-script format:stylelint 'stylelint --fix srcstylelint --fix src'
+npm set-script format:stylelint 'stylelint --fix src'
 npm set-script prepare 'husky install'
 yarn run prepare
 npx husky add .husky/commit-msg  'npx --no -- commitlint --edit ${1}'
